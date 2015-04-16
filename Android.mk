9ce6a65e975bf6da24dc53b504622f40e6c170a6
Tom Marshall <tdm@cyngn.com>
2015-04-15 22:19:28 -0700

    Android: Declare both static and dynamic executables

--- Android.mk
+++ Android.mk
@@ -14,10 +14,21 @@ LOCAL_SRC_FILES := util.c readline/readline.c crc32.c gpt.c
 include $(BUILD_STATIC_LIBRARY)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := gpted
+LOCAL_MODULE := gpted_recovery
+LOCAL_MODULE_STEM := gpted
 LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := RECOVERY_EXECUTABLES
+LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/sbin
 LOCAL_SRC_FILES := gpted.c
 LOCAL_CFLAGS := $(common_cflags)
 LOCAL_FORCE_STATIC_EXECUTABLE := true
 LOCAL_STATIC_LIBRARIES := libc libgpt
+include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := gpted
+LOCAL_MODULE_TAGS := optional
+LOCAL_SRC_FILES := gpted.c
+LOCAL_CFLAGS := $(common_cflags)
+LOCAL_STATIC_LIBRARIES := libgpt
 include $(BUILD_EXECUTABLE)
